9c5655ae75507c970c98de60f2e591f633f7df52
Rob <robpolisena@gmail.com>
2020-06-30 19:17:27 +0000

    Simple Timer

--- timer1.js
+++ timer1.js
@@ -1,10 +1,10 @@
 let input = process.argv.slice(2);
 // input X 1000 to equal seconds
-let timeInput = input.map(x => x * 1000)
+let timeInput = input.map(x => x * 1000);
 const beep = (nums) => {
   for (const num of nums) {
-// if No numbers are provided || An input is a negative number || An input is not a number
-    if(num === null || num < 0 || isNaN(num)) {
+    // if No numbers are provided || An input is a negative number || An input is not a number
+    if (num === null || num < 0 || isNaN(num)) {
       process.exit();
     }
     setTimeout(() => process.stdout.write('\x07'), num);
